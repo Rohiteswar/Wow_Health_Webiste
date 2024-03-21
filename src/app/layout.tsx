bfde9bf1fa956e09121624267d5d4f4d44cfee2a
Rohiteswar velagapudi <rohiteswar.velagapudi@gmail.com>
2024-03-21 08:54:19 +0530

    debug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={myFont.className}>
+      <body className={inter.className}>
         <Navbar />
         {children}
         <div className="static bottom-0">
